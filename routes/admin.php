c244a0c7013cd3dc97f6a76cb3e680c4a69dfb28
Alfawakhry1 <eng.taha1608@gmail.com>
2025-05-10 17:21:43 +0300

    add middleware of admin pages

--- routes/admin.php
+++ routes/admin.php
@@ -2,123 +2,127 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\TestController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
 
 Route::controller(AdminController::class)->group(function () {
-    //Main Page
-    Route::get('admin/dashboard', 'index');
+    Route::middleware( 'auth' ,IsAdmin::class)->group(function () {
 
-    ##****************Start Manage Members****************************#
-    //Show Members
-    Route::get('admin/allmembers', 'AllMembers');
-    Route::get('admin/show/member/{id}', 'show');
+        //Main Page
+        Route::get('admin/dashboard', 'index');
 
-
-    //Add new member
-    Route::get('admin/AddMember', 'AddMember');
-    Route::post('admin/addMember', 'store')->name('store');
+        ##****************Start Manage Members****************************#
+        //Show Members
+        Route::get('admin/allmembers', 'AllMembers');
+        Route::get('admin/show/member/{id}', 'show');
 
 
-    //edit Member
-    Route::get('admin/edit/member/{id}', 'EditForm');
-    Route::put('admin/edit/member/{id}', 'EditMember')->name('UpdateMember');
-
-    //delete member
-    Route::delete('admin/delete/member/{id}', 'DeleteMember')->name('DeleteMember');
-
-    //show trashed member
-    Route::get('admin/all/trashed/members', 'TrashedMembers');
-
-    //restore member
-    Route::get('admin/restore/member/{id}', 'RestoreMember')->name('RestoreMember');
-    //permenent delete member
-
-    Route::delete('admin/pdelete/member/{id}', 'PDeleteMember')->name('PDeleteMember');
-    ##****************End Manage Members****************************#
-    /***************************************************************/
+        //Add new member
+        Route::get('admin/AddMember', 'AddMember');
+        Route::post('admin/addMember', 'store')->name('store');
 
 
-    ##**************** Start Manage Student****************************#
+        //edit Member
+        Route::get('admin/edit/member/{id}', 'EditForm');
+        Route::put('admin/edit/member/{id}', 'EditMember')->name('UpdateMember');
 
-    //Show Students
-    Route::get('admin/allstudents', 'AllStudents');
+        //delete member
+        Route::delete('admin/delete/member/{id}', 'DeleteMember')->name('DeleteMember');
+
+        //show trashed member
+        Route::get('admin/all/trashed/members', 'TrashedMembers');
+
+        //restore member
+        Route::get('admin/restore/member/{id}', 'RestoreMember')->name('RestoreMember');
+        //permenent delete member
+
+        Route::delete('admin/pdelete/member/{id}', 'PDeleteMember')->name('PDeleteMember');
+        ##****************End Manage Members****************************#
+        /***************************************************************/
 
 
+        ##**************** Start Manage Student****************************#
 
-
-    ##**************** End Manage Student****************************#
-    /***************************************************************/
-
-
-    ##**************** Start Manage Trainer ****************************#
-    //Show Trainer
-    Route::get('admin/alltrainers', 'AllTrainers');
-
-
-    ##**************** End Manage Trainer ****************************#
-    /***************************************************************/
+        //Show Students
+        Route::get('admin/allstudents', 'AllStudents');
 
 
 
-    ##**************** Start Manage Categories****************************#
-    //show Categories
-    Route::get('admin/allcategories', 'AllCategories');
-    Route::get('admin/show/category/{id}', 'ShowCategory');
 
-    //add Category
-    Route::get('admin/add/category', 'AddCategoryForm');
-    Route::post('admin/add/category', 'StoreCategory')->name('StoreCategory');
+        ##**************** End Manage Student****************************#
+        /***************************************************************/
 
 
-    //edit category
-    Route::get('admin/edit/category/{id}', 'EditCategoryForm');
-    Route::put('admin/edit/category/{id}', 'UpdateCategory')->name('UpdateCategory');
-
-    //courses related to category
-    Route::get('admin/show/category/courses/{id}', 'ShowCategoryCourses');
-
-    Route::delete('admin/delete/category/{id}', 'DeleteCategory')->name('DeleteCategory');
-
-    //show deleted Categories
-    Route::get('admin/all/trashed/categories', 'TrashedCategories');
-
-    //restore trashed course
-    Route::get('admin/restore/category/{id}', 'RestoreCategory')->name('RestoreCategory');
-
-    //Permenent Delete
-    Route::delete('admin/pdelete/category/{id}', 'PDeleteCategory')->name('PDeleteCategory');
-    ##**************** End Manage Categories****************************#
-    /***************************************************************/
+        ##**************** Start Manage Trainer ****************************#
+        //Show Trainer
+        Route::get('admin/alltrainers', 'AllTrainers');
 
 
-    ##**************** Start Manage Courses****************************#
-    //Courses
-    //show
-    Route::get('admin/allcourses', 'AllCourses');
-    Route::get('admin/show/course/{id}', 'ShowCourse');
-
-    //add
-    Route::get('admin/add/course', 'AddCourseForm');
-    Route::post('admin/add/course', 'StoreCourse')->name('StoreCourse');
+        ##**************** End Manage Trainer ****************************#
+        /***************************************************************/
 
 
-    //edit
-    Route::get('admin/edit/course/{id}', 'EditCourseForm');
-    Route::put('admin/edit/course/{id}', 'UpdateCourse')->name('UpdateCourse');
 
-    //delete
-    Route::delete('admin/delete/course/{id}', 'DeleteCourse')->name('DeleteCourse');
+        ##**************** Start Manage Categories****************************#
+        //show Categories
+        Route::get('admin/allcategories', 'AllCategories');
+        Route::get('admin/show/category/{id}', 'ShowCategory');
 
-    //show deleted course
-    Route::get('admin/all/trashed/courses', 'TrashedCourses');
+        //add Category
+        Route::get('admin/add/category', 'AddCategoryForm');
+        Route::post('admin/add/category', 'StoreCategory')->name('StoreCategory');
 
-    //restore trashed course
-    Route::get('admin/restore/course/{id}', 'RestoreCourse')->name('RestoreCourse');
 
-    //Permenent Delete
-    Route::get('admin/pdelete/course/{id}', 'ForceDeleteCourse')->name('ForceDeleteCourse');
+        //edit category
+        Route::get('admin/edit/category/{id}', 'EditCategoryForm');
+        Route::put('admin/edit/category/{id}', 'UpdateCategory')->name('UpdateCategory');
 
-    ##**************** End Manage Courses****************************#
-    /***************************************************************/
+        //courses related to category
+        Route::get('admin/show/category/courses/{id}', 'ShowCategoryCourses');
+
+        Route::delete('admin/delete/category/{id}', 'DeleteCategory')->name('DeleteCategory');
+
+        //show deleted Categories
+        Route::get('admin/all/trashed/categories', 'TrashedCategories');
+
+        //restore trashed course
+        Route::get('admin/restore/category/{id}', 'RestoreCategory')->name('RestoreCategory');
+
+        //Permenent Delete
+        Route::delete('admin/pdelete/category/{id}', 'PDeleteCategory')->name('PDeleteCategory');
+        ##**************** End Manage Categories****************************#
+        /***************************************************************/
+
+
+        ##**************** Start Manage Courses****************************#
+        //Courses
+        //show
+        Route::get('admin/allcourses', 'AllCourses');
+        Route::get('admin/show/course/{id}', 'ShowCourse');
+
+        //add
+        Route::get('admin/add/course', 'AddCourseForm');
+        Route::post('admin/add/course', 'StoreCourse')->name('StoreCourse');
+
+
+        //edit
+        Route::get('admin/edit/course/{id}', 'EditCourseForm');
+        Route::put('admin/edit/course/{id}', 'UpdateCourse')->name('UpdateCourse');
+
+        //delete
+        Route::delete('admin/delete/course/{id}', 'DeleteCourse')->name('DeleteCourse');
+
+        //show deleted course
+        Route::get('admin/all/trashed/courses', 'TrashedCourses');
+
+        //restore trashed course
+        Route::get('admin/restore/course/{id}', 'RestoreCourse')->name('RestoreCourse');
+
+        //Permenent Delete
+        Route::get('admin/pdelete/course/{id}', 'ForceDeleteCourse')->name('ForceDeleteCourse');
+
+        ##**************** End Manage Courses****************************#
+        /***************************************************************/
+    });
 });
